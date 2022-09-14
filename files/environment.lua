f3621cbd13330765a1f85939839148cdc9569321
navet <navet@duck.com>
2022-09-13 22:25:26 -0300

    new file environment

---
+++ files/environment.lua
@@ -0,0 +1,4 @@
+return function ()
+    game:GetService"Lighting".EnvironmentDiffuseScale = 0.2
+    game:GetService"Lighting".EnvironmentSpecularScale = 0.6
+end
--- main.lua
+++ main.lua
@@ -1,11 +1,14 @@
 local GITHUB_PAGE = "https://raw.githubusercontent.com/setcvar/comovai/main/files/"
 local EXTENSION = ".lua"
+
 local FILES = {
 	"sunrays",
 	"atmosphere",
 	"bloom",
-	"blur"
+	"blur",
+	"environment.lua"
 }
+
 for key, value in pairs (FILES) do
 	local raw_body = request({Url = GITHUB_PAGE .. value .. EXTENSION, Method = "GET"}).Body
 	local file = loadstring(raw_body)()
