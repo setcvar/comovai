205d1fd99651eed11c45da3e6d1e83a355fe00f4
navet <navet@duck.com>
2022-09-13 22:17:48 -0300

    hu

---
+++ files/sunrays.lua
@@ -0,0 +1,3 @@
+return function ()
+   print("hi")
+end
--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local GITHUB_PAGE = "https://raw.githubusercontent.com/setcvar/comovai/main/files/"
 local EXTENSION = ".lua"
 local FILES = {
-	"test",
+	"sunrays",
 }
 for key, value in pairs (FILES) do
 	local raw_body = request({Url = GITHUB_PAGE .. value .. EXTENSION, Method = "GET"}).Body
